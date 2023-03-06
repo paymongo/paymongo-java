18411a3649171b803191b86d24222e2d5daa702a
traqy-paymongo <traqy@paymongo.com>
2023-03-06 10:55:03 +0800

    Include attach, cancel and capture PI functionality

--- src/main/java/com/paymongo/services/PaymentIntent.java
+++ src/main/java/com/paymongo/services/PaymentIntent.java
@@ -6,6 +6,33 @@ public class PaymentIntent extends BaseService {
 
   public static final String URI = "payment_intents";
 
+  public static PaymentIntentEntity attach(String id, Object payload) {
+    return (PaymentIntentEntity) request(
+      "PaymentIntentEntity",
+      "POST",
+      URI + "/" + id + "/attach",
+      payload
+    );
+  }
+
+  public static PaymentIntentEntity cancel(String id) {
+    return (PaymentIntentEntity) request(
+      "PaymentIntentEntity",
+      "POST",
+      URI + "/" + id + "/cancel",
+      null
+    );
+  }
+
+  public static PaymentIntentEntity capture(String id, Object payload) {
+    return (PaymentIntentEntity) request(
+      "PaymentIntentEntity",
+      "POST",
+      URI + "/" + id + "/capture",
+      payload
+    );
+  }
+
   public static PaymentIntentEntity create(Object payload) {
     return (PaymentIntentEntity) request(
       "PaymentIntentEntity",
