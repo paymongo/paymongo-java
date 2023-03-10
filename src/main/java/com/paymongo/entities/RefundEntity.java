c8abb01fe9c93f015ae4d5d8974a0e292e642426
Alelie Pangilinan <alelie@paymongo.com>
2023-03-10 11:08:59 +0800

    Remove other attributes

--- src/main/java/com/paymongo/entities/RefundEntity.java
+++ src/main/java/com/paymongo/entities/RefundEntity.java
@@ -5,7 +5,6 @@ import java.util.Map;
 public class RefundEntity {
   public String id;
   public int amount;
-  public String balance_transaction_id;
   public String currency;
   public Boolean livemode;
   public Object metadata;
@@ -14,9 +13,7 @@ public class RefundEntity {
   public String payout_id;
   public String reason;
   public String status;
-  public int available_at;
   public int created_at;
-  public Integer refunded_at;
   public int updated_at;
 
   public RefundEntity(Map<String, Object> data) {
@@ -24,7 +21,6 @@ public class RefundEntity {
 
     this.id = (String) data.get("id");
     this.amount = (int) attributes.get("amount");
-    this.balance_transaction_id = (String) attributes.get("balance_transaction_id");
     this.currency = (String) attributes.get("currency");
     this.livemode = (Boolean) attributes.get("livemode");
     this.metadata = attributes.get("metadata");
@@ -33,9 +29,7 @@ public class RefundEntity {
     this.payout_id = (String) attributes.get("payout_id");
     this.reason = (String) attributes.get("reason");
     this.status = (String) attributes.get("status");
-    this.available_at = (int) attributes.get("available_at");
     this.created_at = (int) attributes.get("created_at");
-    this.refunded_at = (Integer) attributes.get("refunded_at");
     this.updated_at = (int) attributes.get("updated_at");
   }
 }
